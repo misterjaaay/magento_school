087996e90c049b0311a8ccc48044cf8c1c43a43c
Aleks Litvinov <odesey2008@yandex.ua>
2015-10-09 00:22:32 +0300

    "Admin section is working now. Add some changes with module name"

--- app/code/local/Itdelight/First/controllers/IndexController.php
+++ app/code/local/Itdelight/First/controllers/IndexController.php
@@ -7,7 +7,7 @@ class Itdelight_First_IndexController extends Mage_Core_Controller_Front_Action
     {
         $this->loadLayout();
         $this->renderLayout();
-       /*      $this->loadLayout();
+            /* $this->loadLayout();
         $layoutHandles = $this->getLayout()->getUpdate()->getHandles();
         echo '<pre>' . print_r($layoutHandles, true) . '</pre>';*/
     }
