356c341a779bed0a873bb792b6630021aed3e083
AleksLi <you@example.com>
2015-10-20 17:51:16 +0300

    Lesson 5: add 3 products with image and price

--- app/code/local/Itdelight/First/Block/Test.php
+++ app/code/local/Itdelight/First/Block/Test.php
@@ -6,4 +6,15 @@ class Itdelight_First_Block_Test extends Mage_Core_Block_Template
     {
         echo "<h1> Test </h1>";
     }
+
+    public function getNumOfProducts($attr, $first, $second, $third) {
+        $_collection = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToSelect('*')
+            ->addFieldToFilter($attr, array(
+                array('eq'=>$first),
+                array('eq'=>$second),
+                array('eq'=>$third)
+            ) );
+        return $_collection;
+    }
 }
--- app/code/local/Itdelight/First/Helper/Data.php
+++ app/code/local/Itdelight/First/Helper/Data.php
@@ -1,8 +1,9 @@
 <?php
 
-// was before
-//class Itdelight_First_Helper_Data extends Mage_Payment_Helper_Data
 class Itdelight_First_Helper_Data extends Mage_Core_Helper_Abstract
 {
-
+    public function getHello()
+    {
+        return "helper is working";
+    }
 }
